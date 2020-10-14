606d5fe1e2efb55e134f8e594b8e6817ae1202a1
Archytwo <artemukum@yandex.ru>
2020-10-15 01:57:07 +0300

    Base gameplay

--- gameobject.py
+++ gameobject.py
@@ -1,21 +1,25 @@
-class GameObject:
+import random
 
-    def __init__(self, im, x, y, vx, vy):
+class GameObject:
+    scr_width = None
+    scr_height = None
+
+    def __init__(self, im, x, y, vx, vy, scr_size):
         self.image = im
         self.pos = im.get_rect().move(x,y)
         self.speed_x = vx
         self.speed_y = vy
+        self.scr_width = scr_size[0]
+        self.scr_height = scr_size[1]
 
     def move(self):
         raise NotImplementedError
 
 
-
 class Paddle(GameObject):
 
-    def __init__(self, im, x, y, vy, scr_height):
-        super().__init__(im, x, y, 0, vy)
-        self.scr_height = scr_height
+    def __init__(self, im, x, y, vy, scr_size):
+        super().__init__(im, x, y, 0, vy, scr_size)
         self.dir = 0
 
     def move(self):
@@ -31,9 +35,33 @@ class Paddle(GameObject):
             self.dir = dir
 
 
-
-
 class Ball(GameObject):
-    pass
 
+    def __init__(self, im, x, y, vx, vy, scr_size):
+        super().__init__(im, x, y, vx, vy, scr_size)
 
+    def move(self):
+        self.pos = self.pos.move(self.speed_x, self.speed_y)
+        if self.pos.bottom > self.scr_height:
+            self.speed_y *= -1
+            self.pos.bottom = self.scr_height
+        elif self.pos.top < 0:
+            self.speed_y *= -1
+            self.pos.top = 0
+
+    def revSpeed(self):
+        self.speed_x *= -1
+
+    def isOffside(self):
+        """return winner player or 0 """
+        if self.pos.right > self.scr_width:
+            return 1
+        if self.pos.left < 0:
+            return 2;
+        return 0
+
+    def reset(self):
+        self.pos.center = (self.scr_width // 2, self.scr_height // 2)
+        speed = [-1, 1]
+        self.speed_x = random.choice(speed)
+        self.speed_y = random.choice(speed)
--- main.py
+++ main.py
@@ -1,48 +1,83 @@
 import sys, pygame
 from gameobject import *
 
-pygame.init()
+class GameController:
+    PADDLE_VEL = 1
+    BALL_VEL = 0.5
+    SIZE = WIDTH, HEIGHT = 800, 600
+    BLACK_C = 0, 0, 0
 
-PLATFORM_VELOCITY = 1
+    PLAYER_1_SCORE = 0
+    PLAYER_2_SCORE = 0
+    def __init__(self):
+        pygame.init()
 
-size = width, height = 800, 600
-speed = [1, 1]
-black = 0, 0, 0
-white = 255, 255, 255
+        self.screen = pygame.display.set_mode(self.SIZE)
 
+        paddle_im = pygame.image.load('platform.bmp').convert()
+        self.player1 = Paddle(paddle_im, 10, self.HEIGHT // 2, GameController.PADDLE_VEL, self.SIZE)
+        self.player2 = Paddle(paddle_im, self.WIDTH - 30, self.HEIGHT // 2, GameController.PADDLE_VEL, self.SIZE)
 
+        ball_im = pygame.image.load('ball.bmp').convert()
+        self.ball = Ball(ball_im, self.WIDTH // 2, self.HEIGHT // 2, 1, 1, self.SIZE)
 
-screen = pygame.display.set_mode(size)
-
-paddle_im = pygame.image.load('platform.bmp').convert()
-player1 = Paddle(paddle_im, 10, height//2, PLATFORM_VELOCITY, height)
-player2 = Paddle(paddle_im, width - 30, height//2, PLATFORM_VELOCITY, height)
-
-while True:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                player1.set_dir(-1)
-            elif event.key == pygame.K_z:
-                player1.set_dir(1)
-            elif event.key == pygame.K_UP:
-                player2.set_dir(-1)
-            elif event.key == pygame.K_DOWN:
-                player2.set_dir(1)
-        elif event.type == pygame.KEYUP:
-            if event.key in (pygame.K_a, pygame.K_z):
-                player1.set_dir(0)
-            if event.key in (pygame.K_UP, pygame.K_DOWN):
-                player2.set_dir(0)
+        self.obj = [self.player1, self.player2, self.ball]
 
 
-    player1.move()
-    player2.move()
+    def checkCollision(self):
 
-    screen.fill(black)
-    screen.blit(player1.image, player1.pos)
-    screen.blit(player2.image, player2.pos)
-    pygame.display.flip()
+        if self.ball.pos.colliderect(self.player1.pos) \
+            or self.ball.pos.colliderect(self.player2.pos):
+            self.ball.revSpeed()
+
+    def run(self):
+        while True:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
+                        self.player1.set_dir(-1)
+                    elif event.key == pygame.K_z:
+                        self.player1.set_dir(1)
+                    elif event.key == pygame.K_UP:
+                        self.player2.set_dir(-1)
+                    elif event.key == pygame.K_DOWN:
+                        self.player2.set_dir(1)
+                elif event.type == pygame.KEYUP:
+                    if event.key in (pygame.K_a, pygame.K_z):
+                        self.player1.set_dir(0)
+                    if event.key in (pygame.K_UP, pygame.K_DOWN):
+                        self.player2.set_dir(0)
+
+            self.checkCollision()
+            for elem in self.obj:
+                elem.move()
+
+            #проверяем победителя
+            winner = self.ball.isOffside()
+            if winner:
+                if winner == 1:
+                    self.PLAYER_1_SCORE += 1
+                else:
+                    self.PLAYER_2_SCORE += 1
+                self.ball.reset()
+                continue
+
+
+            self.screen.fill(GameController.BLACK_C)
+
+            for elem in self.obj:
+                self.screen.blit(elem.image, elem.pos)
+
+            pygame.display.flip()
+
+            pygame.time.delay(2)
+
+if __name__ == '__main__':
+    game = GameController()
+    game.run()
+
+
+
